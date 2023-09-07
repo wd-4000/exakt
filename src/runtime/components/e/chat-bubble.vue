e95257423c731c0713e3dab139f16cae82aba31c
wd4000 <preparefordeallocation@outlook.com>
2023-09-07 19:43:22 +0200

    livetype anim

--- src/runtime/components/e/chat-bubble.vue
+++ src/runtime/components/e/chat-bubble.vue
@@ -1,109 +1,114 @@
 <template>
-    <div class="bubble bottom left">
-       Sorry, Dave. I'm afraid I can't do that.
-    </div>
+  <div class="bubble bottom left live-typing">
+    <div class="content">Sorry, Dave. I'm afraid I can't do that.</div>
+    
+  </div>
 </template>
 <style scoped lang="scss">
-$bottom-triangle-height: 0.30rem;
+$bottom-triangle-height: 0.3rem;
 
 .bubble {
-    --current-color: turquoise;
+  --current-color: turquoise;
+  --text-color: 0,0,0;
 
-    position: relative;
-    font-family: sans-serif;
-    width: fit-content;
-    padding: var(--e-core-padding-x);
-    background: var(--current-color);
-    border-radius: var(--e-rounded-border-radius);
-    text-align: center;
-    color: #000;
-    margin-top: 1rem;
+  position: relative;
+  font-family: sans-serif;
+  width: fit-content;
+  padding: var(--e-core-padding-x);
+  background: var(--current-color);
+  border-radius: var(--e-rounded-border-radius);
+  text-align: center;
+  color: rgb(var(--text-color));
+  margin-top: 1rem;
 
-    &:before {
-        content: "";
-        width: 0px;
-        height: 0px;
-        position: absolute;
+  &:before {
+    content: "";
+    width: 0px;
+    height: 0px;
+    position: absolute;
     animation: anim2 2s ease-in-out infinite;
+  }
 
+  &.right {
+    &:before {
+      left: unset;
+      right: calc($bottom-triangle-height * 2);
+      border-right: calc($bottom-triangle-height * 2) solid var(--current-color);
+      border-left: $bottom-triangle-height solid transparent;
     }
+    animation: enter-right 0.5s ease-in-out;
+  }
 
-    &.right {
-        &:before {
-            left: unset;
-            right: calc($bottom-triangle-height*2);
-            border-right: calc($bottom-triangle-height*2) solid var(--current-color);
-            border-left: $bottom-triangle-height solid transparent;
-        }
+  &.left {
+    &:before {
+      left: calc($bottom-triangle-height * 2);
+      border-left: calc($bottom-triangle-height * 2) solid var(--current-color);
+      border-right: $bottom-triangle-height solid transparent;
     }
+    animation: enter-left 0.5s ease-in-out;
+  }
 
-    &.left {
-        &:before {
-            left: calc($bottom-triangle-height*2);
-            border-left: calc($bottom-triangle-height*2) solid var(--current-color);
-            border-right: $bottom-triangle-height solid transparent;
-        }
+  &.live-typing {
+    .content {
+      animation: live-typing 5s linear infinite;
+      background-clip: text;
+      background: linear-gradient(
+        to right,
+        rgba(var(--text-color), 1) 20%,
+        rgba(var(--text-color), 0.5) 40%,
+        rgba(var(--text-color), 0.5) 60%,
+        rgba(var(--text-color), 1) 80%
+      );
+      -webkit-background-clip: text;
+
+      background-size: 200% auto;
+      color: transparent;
     }
-
-    animation: anim1 2s  ease-in-out infinite;
+  }
 }
 
 .bottom {
-    margin-bottom: $bottom-triangle-height*9;
+  margin-bottom: $bottom-triangle-height * 9;
 
-    &:before {
-        border-top: $bottom-triangle-height solid var(--current-color);
-        border-bottom: calc($bottom-triangle-height*2) solid transparent;
-        left: calc($bottom-triangle-height*2);
-        bottom: calc(-2.5*$bottom-triangle-height);
-    }
+  &:before {
+    border-top: $bottom-triangle-height solid var(--current-color);
+    border-bottom: calc($bottom-triangle-height * 2) solid transparent;
+    left: calc($bottom-triangle-height * 2);
+    bottom: calc(-2.5 * $bottom-triangle-height);
+  }
 }
-
 
 .top {
-    margin-top: $bottom-triangle-height*4;
+  margin-top: $bottom-triangle-height * 4;
 
-    &:before {
-        top: calc(-2.5*$bottom-triangle-height);
-        border-bottom: $bottom-triangle-height solid var(--current-color);
-        border-top: calc($bottom-triangle-height*2) solid transparent;
-    }
-
-    @keyframes anim1 {
-        from {
-            scale: 0.9;
-   
-        }
-15%{
-    scale: 1.05;
+  &:before {
+    top: calc(-2.5 * $bottom-triangle-height);
+    border-bottom: $bottom-triangle-height solid var(--current-color);
+    border-top: calc($bottom-triangle-height * 2) solid transparent;
+  }
 }
-        20% {
-            scale: 1;
-        
+@keyframes enter-left {
+  from {
+    transform: scale(0.8) translateX(-30%) translateY(20%);
+  }
 
-        }
-        100%{
-            scale:1;
-        }
-    
-    }
-    
-    @keyframes anim2 {
-        0% {
-            scale: 1.5;
-   
-        }
-        15%{
-    scale: 1.05;
+  to {
+    transform: scale(1) translateX(0%) translateY(0%);
+  }
 }
-        20% {
-            scale: 1;
-        
+@keyframes enter-right {
+  from {
+    transform: scale(0.8) translateX(30%) translateY(20%);
+  }
 
-        }
-        100%{
-            scale:1;
-        }
-    }
+  to {
+    transform: scale(1) translateX(0%) translateY(0%);
+  }
+}
+
+@keyframes live-typing {
+  to {
+    background-position: -200% center;
+  }
 }
 </style>
