d8b5686831ec185f8a279d549c3e0615698aee17
WD-4000 <preparefordeallocation@outlook.com>
2023-09-13 19:35:48 +0200

    Basic chat UI

--- src/runtime/components/e/chat-renderer.vue
+++ src/runtime/components/e/chat-renderer.vue
@@ -1,20 +1,33 @@
 <template>
     <div class="d-flex flex-column">
+        <e-dropdown :items="items" width="13rem" :visible="status.dropdown.visible" fixed
+            :position="{ x: status.dropdown.x, y: status.dropdown.y }" @update:visible="status.dropdown.visible=$event"></e-dropdown>
 
-        <slot v-for="m in computedMessages" :key="m.id" v-bind="m" />
+        <slot v-for="m in computedMessages" :key="m.id" v-bind="m" :name="m.status === 'system' ? 'system' : 'default'"
+            @click.right.prevent="openDropdown"   @touchstart=""/>
     </div>
 </template>
 <script setup lang="ts">
+import { mdiDelete, mdiFlag, mdiMinusCircle } from "@mdi/js";
+import { reactive, computed, defineProps } from "#imports";
+const status = reactive({ dropdown: { visible: false, x: 0, y: 0 } })
+const items = [{ icon: mdiDelete, name: "Delete" }, { icon: mdiMinusCircle, name: "Delete just for me" }, { icon: mdiFlag, name: "Dox" }]
+
+const openDropdown = ({ clientX, clientY }) => {
+    status.dropdown.visible = true;
+    status.dropdown.x = clientX;
+    status.dropdown.y = clientY;
+
+}
 type MessageArray = {
-        liveTyping?: boolean;
-        pointer?: boolean;
-        id: string;
-        text: string;
-        date?: Date;
-        status: "sending" | "sent" | "read" | "received" |"system";
-        [key: string]: any;
-    }[]
-import { computed, defineProps } from "#imports";
+    liveTyping?: boolean;
+    pointer?: boolean;
+    id: string;
+    text: string;
+    date?: Date;
+    status: "sending" | "sent" | "read" | "received" | "system";
+    [key: string]: any;
+}[]
 const props = defineProps<{
     messages: MessageArray
 }>()
@@ -34,29 +47,30 @@ const computedMessages = computed(() => {
             messages[i].pointer = true
         }
 
-        if(m.status == "system"){
-    
+        if (m.status == "system") {
+
         }
 
-        switch(m.status){
+        switch (m.status) {
             case "system":
-            newMessages[i].hideStatus = true;
-            newMessages[i].pointer = false;
-            newMessages[i].posx = "center";
-            newMessages[i].textAlign= "center";
+                newMessages[i].hideStatus = true;
+                newMessages[i].pointer = false;
+                newMessages[i].posx = "center";
+                newMessages[i].textAlign = "center";
                 break;
             case "received":
- 
-            newMessages[i].posx = "left";
+
+                newMessages[i].posx = "left";
                 break;
             default:
-            newMessages[i].posx = "right";
+                newMessages[i].posx = "right";
                 break;
         }
 
 
 
 
-       
-    } return newMessages})
+
+    } return newMessages
+})
 </script>
