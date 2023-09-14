811ba65a1c5d41886f28c435c7394fcc1b330e2f
wd4000 <preparefordeallocation@outlook.com>
2023-09-14 23:56:37 +0200

    improve e-avatar

--- src/runtime/components/e/avatar.vue
+++ src/runtime/components/e/avatar.vue
@@ -1,31 +1,20 @@
 <template>
   <div class="flex-center a-container">
     <slot />
-    <e-image-lazy-view
-      v-if="src"
-      class="avatar"
-      :width="size"
-      :height="size"
-      :contain="true"
-      :immediate-render="true"
-      :src="src"
-      border-radius="100%"
-    />
+    <e-image-lazy-view v-if="src" class="avatar" :width="props.size" :height="props.size" :contain="true"
+      :immediate-render="true" :src="src" border-radius="100%" />
     <div v-else>
-      <e-icon
-        :size="size"
-        :icon="mdiAccountCircle"
-      />
+      <e-icon :size="props.size" :icon="mdiAccountCircle" />
     </div>
   </div>
 </template>
 <script setup lang="ts">
 import { mdiAccountCircle } from "@mdi/js";
-const size = 120;
 
-defineProps<{
+const props = withDefaults(defineProps<{
   src?: string;
-}>();
+  size?: number;
+}>(), { size: 120 });
 </script>
 <style lang="scss" scoped>
 .avatar {
