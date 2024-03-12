584dc4a02bf9765dacb62f20ffcf57321c642a15
wd4000 <preparefordeallocation@outlook.com>
2024-03-12 12:12:37 +0100

    slightly glitchy double sidebar

--- playground/pages/sidebar.vue
+++ playground/pages/sidebar.vue
@@ -1,24 +1,31 @@
 <template>
-  <e-nav-sidebar collapsed-width="3.5rem">
+  <e-nav-sidebar
+    collapsed-width="3.5rem"
+    expanded-width="15rem"
+    elev="3"
+    
+    :items="[{ icon: mdiHome, title: 'Home', to: '/' }, { icon: mdiAccountMultiple, title: 'Users', to: '/' },{ icon: mdiEthernet, title: 'Network', to: '/sidebar' },{ icon: mdiCalendar, title: 'Events', to: '/' }]"
+  >
     <template #app>
-      <div>Sidebar example</div>
-    </template><template>
-      <e-nav-sidebar-btn
-        title="John Pork"
-        subtitle="hi i'm john pork"
-        :icon="mdiHome"
-        to="/"
-      />
-      <e-nav-sidebar-btn
-        title="John Pork"
-        subtitle="hi i'm john pork"
-        :icon="mdiHome"
-        to="/sidebar"
-      />
+      <e-nav-sidebar
+        collapsed-width="3.5rem"
+        expanded-width="12rem"
+        :move-content="0"
+        :nesting-level="1"
+        :items="[{ icon: mdiDevices, title: 'Devices', to: '/' }, { icon: mdiServer, title: 'Infrastructure', to: '/' }, { icon: mdiWifi, title: 'WLAN', to: '/' }]"
+        elev="2"
+      >
+        <template #app>
+          <div class="bg-red">
+            Sidebar example Sidebar example Sidebar example Sidebar example
+            Sidebar example Sidebar example Sidebar example Sidebar example
+            Sidebar example Sidebar example Sidebar example
+          </div>
+        </template>
+      </e-nav-sidebar>
     </template>
   </e-nav-sidebar>
 </template>
 <script setup lang="ts">
-import  { mdiHome } from '@mdi/js';
-
+import { mdiAccountMultiple, mdiCalendar, mdiDevices, mdiEthernet, mdiHome, mdiServer, mdiWifi } from "@mdi/js";
 </script>
