49ade043592e0bdebbebab9edda8d4423a96431b
jorb <hello@jorb.cc>
2024-07-28 16:57:02 +0200

    upgrade nuxt

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,6 +1,7 @@
 export default defineNuxtConfig({
-  modules: ["../src/module"],
+ modules: ["../src/module"],
  exakt: { font: {family:"Helvetica, 'Segoe UI', sans-serif"}, colors: { primary: "#ff7d00" } },
-  components: true,
-  pages: true
+ components: true,
+ pages: true,
+ compatibilityDate: "2024-07-28"
 });
