047cee28853b721f0f43955f8b7c21b9dd6961ca
jorb <hello@jorb.cc>
2024-09-04 15:10:31 +0200

    feat: hr

---
+++ src/runtime/components/e/hr.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="hr"><slot/></div>
+</template>
+<style scoped>
+.hr {
+  display: flex;
+  align-items: center;
+  text-align: center;
+}
+
+.hr::before,
+.hr::after {
+  content: '';
+  flex: 1;
+  border-bottom: .125em solid var(--e-color-elev);
+}
+
+.hr:not(:empty)::before {
+  margin-right: .75em;
+}
+
+.hr:not(:empty)::after {
+  margin-left: .75em;
+}
+</style>
