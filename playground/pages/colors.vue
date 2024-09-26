19eb69eee726e61c30f7713042aa8efb83b810c3
jorb <hello@jorb.cc>
2024-09-26 22:09:07 +0200

    fix: text input placeholder

--- playground/pages/colors.vue
+++ playground/pages/colors.vue
@@ -1,10 +1,23 @@
 <template>
-<h1>Colors</h1>
-<h2>Background</h2>
-    <div v-for="color in colors" :class="['bg-'+color]">{{ color }}</div>
-<h2>Text</h2>
-<div v-for="color in colors" :class="['color-'+color]">{{ color }}</div>
-
+  <div>
+    <h1>Colors</h1>
+    <h2>Background</h2>
+    <div
+      v-for="color in colors"
+      :key="color"
+      :class="['bg-' + color]"
+    >
+      {{ color }}
+    </div>
+    <h2>Text</h2>
+    <div
+      v-for="color in colors"
+      :key="color"
+      :class="['color-' + color]"
+    >
+      {{ color }}
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
 const colors = ['primary', 'text', 'red', 'blue', 'yellow', 'green', 'bg', 'fg']
