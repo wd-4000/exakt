133a73633a7d4745606a3bd60458dc3ca9e2018d
WD-4000 <preparefordeallocation@outlook.com>
2024-10-07 19:54:03 +0200

    feat: wrap text input to make click listener work

--- src/runtime/components/e/input/text.vue
+++ src/runtime/components/e/input/text.vue
@@ -1,22 +1,24 @@
 <template>
-  <div v-if="label" class="mb-3 mt-6">
-    <label :for="id">
-      {{ label }} </label>
-  </div>
+  <div>
+    <div v-if="label" class="mb-3 mt-6">
+      <label :for="id">
+        {{ label }} </label>
+    </div>
 
-  <div class="wrapper fullwidth" :style="inputState.overtakeStyle"
-    :class="{ rounded: rounded == undefined ? solid : rounded, solid }" @click="focus">
-    <e-icon v-if="icon" class="mr-2" size="19" fill="true">
-      {{ icon }}
-    </e-icon>
-    <textarea v-if="type === 'textarea'" ref="input" v-model="currentText" class="input" :name="name"
-      :placeholder="placeholder" autocomplete="off" auto-grow rows="5" @focus="inputState.focused = true"
-      @blur="inputState.focused = false" />
-    <input v-else :id="id" ref="input" v-model="currentText" :disabled="disabled" :type="type" :name="name"
-      :autocomplete="autocomplete" :spellcheck="spellcheck" class="input" :required="required"
-      :placeholder="placeholder" @click.stop="" @focus="inputState.focused = true" @blur="inputState.focused = false"
-      @transitionend="transitionEnd">
-    <slot />
+    <div class="wrapper fullwidth" :style="inputState.overtakeStyle"
+      :class="{ rounded: rounded == undefined ? solid : rounded, solid }" @click="focus">
+      <e-icon v-if="icon" class="mr-2" size="19" fill="true">
+        {{ icon }}
+      </e-icon>
+      <textarea v-if="type === 'textarea'" ref="input" v-model="currentText" class="input" :name="name"
+        :placeholder="placeholder" autocomplete="off" auto-grow rows="5" @focus="inputState.focused = true"
+        @blur="inputState.focused = false" />
+      <input v-else :id="id" ref="input" v-model="currentText" :disabled="disabled" :type="type" :name="name"
+        :autocomplete="autocomplete" :spellcheck="spellcheck" class="input" :required="required"
+        :placeholder="placeholder" @click.stop="" @focus="inputState.focused = true" @blur="inputState.focused = false"
+        @transitionend="transitionEnd">
+      <slot />
+    </div>
   </div>
 </template>
 <script setup lang="ts">
