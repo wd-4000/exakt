22b2b4b65b9411344ae5547095517fef8a55f3ca
jorb <hello@jorb.cc>
2025-01-20 15:16:34 +0100

    feat: e-input-combo (but it actually kind of works)

--- src/runtime/components/e/input/combo.vue
+++ src/runtime/components/e/input/combo.vue
@@ -1,29 +1,23 @@
 <template>
-  <e-dropdown
-    v-model="dropdownActive"
-    :items="items"
-    width="100%"
-    :center="false"
-    :visible="dropdownVisible"
-    padding-y="0.25rem"
-    @update:visible="dropdownVisible = $event"
-  >
-    <e-input-text
-      v-model="searchField"
-      label="Input"
-      solid
-    />
-  </e-dropdown>
+  <div>
+    <e-dropdown v-model="dropdownActive" :items="items" :visible="disabled? false :dropdownVisible" 
+      @update:visible="dropdownVisible = $event" 
+      @update:model-value="emit('update:modelValue')"
+      v-bind="{ width }"> 
+      <e-input-text v-model="searchField" v-bind="{ label, placeholder, icon}" solid :disabled="disabled" :class="{'e-disabled':props.disabled}"><slot/></e-input-text>
+    </e-dropdown>
+  </div>
 </template>
 <script setup lang="ts">
 import { ref, watch } from "#imports";
-const items = [{ name: "test" }, { name: "test2" }, { name: "test3" }];
 const dropdownActive = ref<undefined | number>(undefined);
 const dropdownVisible = ref<boolean>(false);
-
+const props = defineProps<{ label?: string, placeholder?: string, width?: string, items: { name: string, [key: string]: string }[], modelValue?: number, icon?:string, disabled?:boolean}>()
 const searchField = ref("");
+const emit = defineEmits(["update:modelValue", "update:searchValue"]);
 
 watch(searchField, (value) => {
+  emit("update:searchValue", value)
   if (value.length > 0) {
     //dropdownActive.value = 0;
     dropdownVisible.value = true;
@@ -37,7 +31,7 @@ watch(dropdownActive, (value) => {
   if (value == undefined) {
     searchField.value = "";
   } else {
-    searchField.value = items[value].name;
+    searchField.value = props.items[value].name;
   }
 });
 </script>
