3e2214d2ba2bc2c7cb441ebaee8427b3555f488a
jorb <hello@jorb.cc>
2025-04-22 16:40:01 +0200

    fix: slightly improve dropdown x offset

--- src/runtime/components/e/dropdown.vue
+++ src/runtime/components/e/dropdown.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="flex-stretch t-dropdown">
     <!-- class="flex-stretch fullwidth" -->
-    <div
-      ref="activator"
-      @click="onActivatorClick"
-    >
+    <div ref="activator" @click="onActivatorClick">
       <slot />
     </div>
     <e-focus-sheet v-model="visibleComputed" />
@@ -36,20 +33,13 @@
             }"
             @click="select(i)"
           >
-            <e-icon
-              v-if="item.icon"
-              :size="20"
-              class="mr-2"
-            >
+            <e-icon v-if="item.icon" :size="20" class="mr-2">
               {{ item.icon }}
             </e-icon>
             {{ item.name }}
           </e-btn>
         </component>
-        <div
-          v-if="hint"
-          class="mx-4 my-2 fullwidth text-secondary"
-        >
+        <div v-if="hint" class="mx-4 my-2 fullwidth text-secondary">
           {{ hint }}
         </div>
       </div>
@@ -150,7 +140,8 @@ watch(
 
     if (newList && activatorRect) {
       if (window?.innerWidth < newList.getBoundingClientRect().right) {
-        state.x = -1 * activatorRect.width * 2;
+        state.x =
+          window?.innerWidth - newList.getBoundingClientRect().right - 15;
       }
     }
   },
