aa47da884bffe7673d8b37a6c21991fbc952e358
WD-4000 <preparefordeallocation@outlook.com>
2025-04-27 15:49:24 +0200

    feat: select

---
+++ src/runtime/components/e/input/select.vue
@@ -0,0 +1,57 @@
+<template>
+  <e-dropdown
+    :items="items"
+    :visible="focus"
+    :modelValue="modelValue"
+    @update:model-value="modelValue = $event"
+    @update:visible="focus = $event"
+  >
+    <e-btn background="i-depressed" :class="{ focus }" class="btn">
+      <div class="d-flex justify-space-between">
+        <div
+          v-if="modelValue != undefined && items[modelValue]"
+          class="flex-center"
+        >
+          <e-icon size="20">{{ items[modelValue].icon }}</e-icon>
+          <div class="ml-2 md-and-over">{{ items[modelValue].name }}</div>
+        </div>
+        <div v-else></div>
+        <e-icon class="text-secondary ml-1" size="20">arrow_drop_down</e-icon>
+      </div></e-btn
+    ></e-dropdown
+  >
+</template>
+<script setup lang="ts">
+import { ref, reactive } from "#imports";
+const focus = ref(false);
+const modelValue = ref(undefined);
+
+interface DropdownItem {
+  name: string;
+  icon?: string;
+  href?: string;
+  to?: string;
+  callback?: () => void;
+  color?: string;
+  background?: string;
+}
+
+const props = withDefaults(
+  defineProps<{
+    items: DropdownItem[] | [];
+  }>(),
+  { items: () => [] },
+);
+</script>
+<style lang="scss" scoped>
+.btn {
+  outline: var(--e-color-i-outline) solid 0.1rem;
+  transition:
+    outline ease-in-out 0.15s,
+    background-color ease-in-out 0.15s;
+  margin: 0.125rem;
+  &.focus {
+    outline: var(--e-color-primary) solid 0.125rem;
+  }
+}
+</style>
