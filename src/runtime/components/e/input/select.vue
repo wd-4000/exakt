a82b6b74d359d4d6f28f241ac6a94c4d3cd8ec00
jorb <hello@jorb.cc>
2025-05-02 00:22:00 +0200

    feat: support IDs in e-dropdown and e-input-select

--- src/runtime/components/e/input/select.vue
+++ src/runtime/components/e/input/select.vue
@@ -1,28 +1,30 @@
 <template>
+  <div v-if="label" class="my-3">
+    <label> {{ label }} </label>
+  </div>
   <e-dropdown
     :items="items"
     :visible="focus"
     :model-value="props.modelValue"
     @update:model-value="emit('update:modelValue', $event)"
     @update:visible="focus = $event"
+    :use-ids="useIds"
+    v-slot="{ currentItem }"
   >
-    <e-btn background="i-depressed" :class="{ focus }" class="btn">
+    <div :class="{ focus }" class="btn rounded">
       <div class="d-flex justify-space-between">
-        <div
-          v-if="modelValue != undefined && items[modelValue]"
-          class="flex-center"
-        >
+        <div v-if="currentItem" class="flex-center">
           <e-icon size="20">
-            {{ items[modelValue].icon }}
+            {{ currentItem?.icon }}
           </e-icon>
           <div class="ml-2 md-and-over">
-            {{ items[modelValue].name }}
+            {{ currentItem?.name }}
           </div>
         </div>
         <div v-else />
         <e-icon class="text-secondary ml-1" size="20"> arrow_drop_down </e-icon>
       </div>
-    </e-btn>
+    </div>
   </e-dropdown>
 </template>
 <script setup lang="ts">
@@ -44,9 +46,16 @@ const emit = defineEmits(["update:modelValue"]);
 const props = withDefaults(
   defineProps<{
     items: DropdownItem[] | [];
+    label?: string;
+    useIds?: boolean;
     modelValue: number | undefined;
   }>(),
-  { items: () => [], modelValue: undefined },
+  {
+    items: () => [],
+    modelValue: undefined,
+    label: undefined,
+    useIds: false,
+  },
 );
 </script>
 <style lang="scss" scoped>
@@ -56,6 +65,10 @@ const props = withDefaults(
     outline ease-in-out 0.15s,
     background-color ease-in-out 0.15s;
   margin: 0.125rem;
+  user-select: none;
+  cursor: pointer;
+  padding: 0.7rem 0.9rem;
+  background-color: var(--e-color-i-depressed);
   &.focus {
     outline: var(--e-color-primary) solid 0.125rem;
   }
