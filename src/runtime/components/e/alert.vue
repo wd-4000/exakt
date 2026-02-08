92477beba962f474c60060f99f571814ed062b52
WD-4000 <preparefordeallocation@outlook.com>
2026-02-08 22:12:13 +0100

    feat: don't horizontally align e-alert, tweak stage 2 icon

--- src/runtime/components/e/alert.vue
+++ src/runtime/components/e/alert.vue
@@ -1,20 +1,23 @@
 <template>
   <div
-    class="flex-center"
+    class="flex-align-center"
     :class="{ [severityMatrix[severity].class]: true }"
   >
-    <e-icon
-      size="19"
-      class="mr-2"
-    >
-      {{ severityMatrix[severity].icon }}
-    </e-icon><small>
+    <e-icon size="19" class="mr-2" :fill="false">
+      {{ severityMatrix[severity].icon }} </e-icon
+    ><small>
       <slot />
     </small>
   </div>
 </template>
 <script setup lang="ts">
-withDefaults(defineProps<{ severity?: 0 | 1 | 2 }>(), { severity: 2 })
+withDefaults(defineProps<{ severity?: 0 | 1 | 2 }>(), {
+  severity: 2,
+});
 
-const severityMatrix = { 0: { icon: "info", class: "" }, 1: { icon: "warning", class: "color-yellow" }, 2: { icon: "warning", class: "color-red" } }
+const severityMatrix = {
+  0: { icon: "info", class: "" },
+  1: { icon: "warning", class: "color-yellow" },
+  2: { icon: "dangerous", class: "color-red" },
+};
 </script>
